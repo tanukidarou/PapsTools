ae6790ba8f05c506556caca599dacabe71e07ce4
CiarlantiniD <dario.ciarlantini@outlook.com>
2019-07-23 19:05:43 -0300

    Se agrego el BuildVersion Display

---
+++ Paps/Paps/Paps/Unity/BuildVersion.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scratch
+{
+    [RequireComponent(typeof(Text))]
+    public class BuildVersion : MonoBehaviour
+    {
+
+        [SerializeField] private string androidVersion;
+
+#if UNITY_STANDALONE_WIN || UNITY_WEBGL
+        
+        void Awake()
+        {
+            string guid = "";
+            if (!string.IsNullOrEmpty(Application.buildGUID))
+            {
+                guid = Application.buildGUID.Substring(0,5);
+            }
+            else
+            {
+                guid = "editor";
+            }
+            GetComponent<Text>().text = "v" + Application.version + "." +guid;
+        }
+
+#elif  UNITY_ANDROID
+
+        void Awake()
+        {
+            string guid = "";
+            if (!string.IsNullOrEmpty(Application.buildGUID))
+            {
+                guid = Application.buildGUID.Substring(0, 5);
+            }
+            else
+            {
+                guid = "editor";
+            }
+            GetComponent<Text>().text = "v" + androidVersion + "." + guid;
+        }
+#endif
+
+    }
+}
+
+
